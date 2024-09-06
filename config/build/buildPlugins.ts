efa455438342ad12ec6dc44ffecfae7708db1c03
Daniil <temnay2@icloud.com>
2024-09-06 22:43:33 +0300

    configured basic webpack configuration, eslint and stylelint

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,23 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { BuildOptions } from './types/config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+
+export const BuildPlugins = ({ paths, isDev, apiUrl }: BuildOptions) => {
+
+	return [
+		new HtmlWebpackPlugin({
+			template: paths.html
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash:8].css',
+			chunkFilename: 'css/[name].[contenthash:8].css'
+		}),
+		new webpack.DefinePlugin({
+			__IS_DEV__: JSON.stringify(isDev),
+			__API_URL__: JSON.stringify(apiUrl),
+		}),
+		new webpack.ProgressPlugin(),
+		new webpack.HotModuleReplacementPlugin()
+	];
+};
---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/index.scss';
+
+const App = () => {
+	return (
+		<div className={'App'}>
+			Hello, Avito!
+		</div>
+	);
+};
+
+export default App;
