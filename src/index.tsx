a1e07f23a08142cad1710bd23e848b84308e4188
Daniil <temnay2@icloud.com>
2024-09-12 23:55:36 +0300

    added error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { StoreProvider } from 'app/providers/StoreProvider';
 import App from './app/App';
 
@@ -8,7 +9,9 @@ const root = createRoot(domNode as HTMLElement);
 root.render(
 	<StoreProvider>
 		<BrowserRouter>
-			<App/>
+			<ErrorBoundary>
+				<App/>
+			</ErrorBoundary>
 		</BrowserRouter>
 	</StoreProvider>
 );
